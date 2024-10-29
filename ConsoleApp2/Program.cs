0bb3658d04789f2bd221a71af00b3b4450d06bba
IvanDolomanji <156667166+IvanDolomanji@users.noreply.github.com>
2024-10-29 11:17:22 +0200

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //задание 1 //Заполнить одномерный массив из 8 целых чисел с клавиатуры и вывести его на экран.
+            //int[] omas = new int[8];
+            //Console.WriteLine("Заполнение однмерного массива");
+            //for (int i = 0; i < 8; i++)
+            //{
+            //    Console.Write($"omas[i]");
+            //    omas [i] = int.Parse( Console.ReadLine());
+            //}
+            //Console.WriteLine("Вывод одномерного массива"); 
+            //for (int i = 0; i < 8; i++)
+            //{ 
+            //    Console.WriteLine($"omas[{i}] = {omas[i]}");
+            //}
+            //Console.Read();
+
+            //задача 2
+            // Составьте программу ввода (без клавиатуры) 
+            // и вывода массива: 5, -2, 3, -6, 7, 9, -4, 8, 1, 2, 7, -3.
+            //int[] omas = new int[12]
+            //{ 5, -2, 3, -6, 7, 9, -4, 8, 1, 2, 7, -3}; 
+            //Console.WriteLine("Вывод одномерного массива");
+            //for (int i = 0; i < 12; i++) 
+            //{
+            //    Console.WriteLine($"omas[{i}] = {omas[i]}");
+            //}
+            //Console.Read();
+
+            //задача 3 
+            //Массив предназначен для хранения значений ростов двенадцати человек.
+            //С помощью датчика случайных чисел заполнить массив целыми значениями,
+            //лежащими в диапазоне от 163 до 190 включительно.
+            //int[] omas = new int[12];
+            //Random rnd = new Random(); 
+            //for (int i = 0; i < omas.Length; i++)
+            //{ 
+            //    omas [i] = rnd.Next(163,190);
+            //} 
+            //Console.WriteLine("Вывод одномерного массива");
+            //for (int i = 0; i < 12; i++)
+            //{
+            //    Console.WriteLine($"omas[{i}] = {omas[i]}");
+            //}
+            //Console.Read(); 
+
+            //та же задача но с температурой
+            //int[] omas = new int[31];
+            //Random rnd = new Random(); 
+            //for (int i = 0; i < omas.Length; i++)
+            //{ 
+            //    omas[i] = rnd.Next(5, 25);
+            //}
+            //Console.WriteLine("Вывод одномерного массива");
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            //    Console.WriteLine($"omas[{i}] = {omas[i]}");
+            //}
+            //int sum = 0;
+            //for (int i = 0; i < omas.Length; i++)
+            //{ 
+            //    sum = sum + omas[i];
+            //   
+            //sum + = omas[i];
+            //}
+            //Console.WriteLine($"Средняя температура = {sum / omas.Length}"); 
+            //Console.Read();
+        }
+    }
+}
